e9c6943a1a2b17d8c0bf4543c86d419ed481d7c5
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-01-11 20:35:53 +0600

    added new files and done something

--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { add } from '../utilities/Calculate';
 
 const Cosmetics = () => {
+    const first = 56;
+    const second = 45;
+    const total = add(first, second);
     return (
         <div>
             <h1>Welcome to my cosmetics store</h1>
+            <p>Total: {total}</p>
         </div>
     );
 };
--- src/components/Shoes/Shoes.js
+++ src/components/Shoes/Shoes.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { multiply } from '../utilities/Calculate';
 
 const Shoes = () => {
+    const first = 15;
+    const second = 12;
+    const result = multiply(first, second);
     return (
         <div>
             <h1>This is Shoes components</h1>
+            <p>Result: {result}</p>
         </div>
     );
 };
