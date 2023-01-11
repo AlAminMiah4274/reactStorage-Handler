090fd224a91a61880665ef70616218552594b7c9
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-01-11 21:54:01 +0600

    added style on cosmetic file

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Cosmetic.css';
 
 const Cosmetic = (props) => {
     const { name, price, id } = props.cosmetic;
     return (
-        <div>
+        <div className='product'>
             <h2>Buy this: {name}</h2>
             <p>Only for ${price}</p>
             <p>It has id: {id}</p>
