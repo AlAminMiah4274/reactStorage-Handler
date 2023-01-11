ca8343cecea16beffebba008a2936587513edbd1
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-01-11 22:19:46 +0600

    added a function named addToCart for getting the id

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -3,11 +3,17 @@ import './Cosmetic.css';
 
 const Cosmetic = (props) => {
     const { name, price, id } = props.cosmetic;
+
+    const addToCart = (id) => {
+
+    }
+
     return (
         <div className='product'>
             <h2>Buy this: {name}</h2>
             <p>Only for ${price}</p>
             <p>It has id: {id}</p>
+            <button onClick={() => addToCart(id)}>Add to Cart</button>
         </div>
     );
 };
